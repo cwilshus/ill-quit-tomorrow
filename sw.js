6c6ec6568253585720e2eb884a502ab265b06394
Caleb Wilshusen <cwilshusen@turo.com>
2026-04-13 20:54:44 -0700

    sw: force page reload on activate so phone always gets fresh code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'iqt-v5';
+const CACHE = 'iqt-v6';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
@@ -12,6 +12,8 @@ self.addEventListener('activate', e => {
     caches.keys()
       .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
 
